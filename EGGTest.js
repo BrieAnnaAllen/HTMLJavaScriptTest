2d918abb2af00802b5cdc9ee0f99f79ab5d509bf
BrieAnnaAllen <BrieAnnaNAllen@gmail.com>
2022-09-17 14:12:40 -0500

    First upload

---
+++ EGGTest.js
@@ -0,0 +1,10 @@
+function setTitle(titleSelect){
+    var val;
+    switch(titleSelect.options[titleSelect.selectedIndex].value) {
+        case 'strong-brute': val = "STRONG | BRUTE"; break;
+        case 'tough-commando': val = "TOUGH | COMMANDO"; break;
+        case 'smart-mastermind': val = "SMART | MASTERMIND"; break;
+        case 'wise-sleuth': val = "WISE | SLEUTH"; break;
+    }
+    document.getElementById('class-title').value = val;
+}
